7acc33bbc43d0f070fa07c0275162f76c1f4a404
Jason Dreyzehner <jason@dreyzehner.com>
2019-08-03 03:14:00 +0200

    feat(schnorr): port Bitcoin ABC schnorr module

--- src/tests.c
+++ src/tests.c
@@ -6864,6 +6864,10 @@ void run_ecdsa_edge_cases(void) {
 # include "modules/recovery/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_SCHNORR
+# include "modules/schnorr/tests_impl.h"
+#endif
+
 #ifdef ENABLE_MODULE_EXTRAKEYS
 # include "modules/extrakeys/tests_impl.h"
 #endif
@@ -7162,6 +7166,11 @@ int main(int argc, char **argv) {
 #ifdef ENABLE_MODULE_RECOVERY
     /* ECDSA pubkey recovery tests */
     run_recovery_tests();
+#endif
+
+#ifdef ENABLE_MODULE_SCHNORR
+    /* Schnorr signature tests */
+    run_schnorr_tests();
 #endif
 
 #ifdef ENABLE_MODULE_EXTRAKEYS
